58b5ea06ff500fef05bb805f5549ced3f0a517b1
MenekseYuncu <menekseyuncu@gmail.com>
2024-11-17 14:29:32 +0300

    base responses added

--- src/main/java/com/menekseyuncu/storemanagement/common/responses/BaseResponse.java
+++ src/main/java/com/menekseyuncu/storemanagement/common/responses/BaseResponse.java
@@ -1,4 +1,34 @@
 package com.menekseyuncu.storemanagement.common.responses;
 
-public class BaseResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class BaseResponse<T> {
+
+    public static final BaseResponse<Void> SUCCESS = BaseResponse.<Void>builder()
+            .httpStatus(HttpStatus.OK)
+            .isSuccess(true).build();
+
+    @Builder.Default
+    private LocalDateTime time = LocalDateTime.now();
+
+    private HttpStatus httpStatus;
+
+    private Boolean isSuccess;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T response;
+
+    public static <T> BaseResponse<T> successOf(final T response) {
+        return BaseResponse.<T>builder()
+                .httpStatus(HttpStatus.OK)
+                .isSuccess(true)
+                .response(response).build();
+    }
 }
--- src/main/java/com/menekseyuncu/storemanagement/common/responses/ErrorResponse.java
+++ src/main/java/com/menekseyuncu/storemanagement/common/responses/ErrorResponse.java
@@ -1,4 +1,16 @@
 package com.menekseyuncu.storemanagement.common.responses;
 
-public class ErrorResponse {
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse extends BaseResponse<String> {
+
+    public ErrorResponse(HttpStatus httpStatus, String message) {
+        super(LocalDateTime.now(), httpStatus, false, message);
+    }
+
+    public static ErrorResponse failureOf(HttpStatus httpStatus, String message) {
+        return new ErrorResponse(httpStatus, message);
+    }
 }
